071f5eac2afc04593683c971483cf4a6a296e22b
bmqy <qq88268459@126.com>
2021-12-21 08:32:19 +0800

    test

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def get_posts():
         post_link_id_list.append({
             "id": post.id,
             "link": post.link,
-            "date": time.strftime("%Y%m%d",post.date)
+            "date": time.strftime("%Y%m%d",post.date.datetime)
         })
     print(post_link_id_list)
     print(len(post_link_id_list))
