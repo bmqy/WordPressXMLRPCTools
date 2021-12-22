c8a984aec17a3634939c0ea3e344910955240fc5
bmqy <qq88268459@126.com>
2021-12-22 13:51:46 +0800

    test

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def get_posts():
     for post in posts:
         post_title_id_list.append({
             "id": post.id,
-            "title": post.date.strftime("%Y%m") +'-'+ post.title,
+            "title": post.date.strftime("%Y%m") +'###'+ post.title,
         })
     print(post_title_id_list)
     print(len(post_title_id_list))
@@ -122,11 +122,12 @@ def edit_post(id, title, content, link, post_status, terms_names_post_tag, terms
 def read_md(file_path):
     content = ""
     metadata = {}
-    get_md_path_title(file_path)
+    path_title = get_md_path_title(file_path)
     with open(file_path) as f:
         post = frontmatter.load(f)
         content = post.content
         metadata = post.metadata
+        pathTitle = path_title
         # print("==>>", post.content)
         # print("===>>", post.metadata)
     return (content, metadata)
@@ -139,7 +140,7 @@ def get_md_path_title(path):
     file = pathArr[length - 1]
     file = os.path.splitext(file)[0]
     folder = pathArr[length - 2]
-    md_path_title = folder +'-'+ file
+    md_path_title = folder +'###'+ file
     print(md_path_title)
     return md_path_title
 
@@ -225,7 +226,7 @@ def href_info(link):
     return "<br/><br/><br/>\n\n\n\n## 本文永久更新地址: \n[" + link + "](" + link + ")"
 
 # 在README.md中插入信息文章索引信息，更容易获取google的收录
-def insert_index_info_in_readme():
+def insert_index_info_in_readme(title_id_dic):
     # 获取_posts下所有markdown文件
     md_list = get_md_list(os.path.join(os.getcwd(), "_posts"))
     # 生成插入列表
@@ -233,25 +234,26 @@ def insert_index_info_in_readme():
     md_list.sort(reverse=True)
     # 读取md_list中的文件标题
     for md in md_list:
-        (content, metadata) = read_md(md)
+        (content, metadata, pathTitle) = read_md(md)
+        id = title_id_dic[pathTitle]
         title = metadata.get("title", "")
-        insert_info = insert_info + "[" + title +"](" + "https://"+domain_name + "/p/" + os.path.basename(md).split(".")[0] +"/" + ")\n\n"
+        insert_info = insert_info + "[" + title +"](" + "https://"+domain_name + "/" + id +"html" + ")\n\n"
     # 替换 ---start--- 到 ---end--- 之间的内容
 
     insert_info = "---start---\n## 目录(" + time.strftime('%Y年%m月%d日') + "更新)" +"\n" + insert_info + "---end---"
 
     # 获取README.md内容
-    with open (os.path.join(os.getcwd(), "README.md"), 'r', encoding='utf-8') as f:
-        readme_md_content = f.read()
+    # with open (os.path.join(os.getcwd(), "README.md"), 'r', encoding='utf-8') as f:
+    #     readme_md_content = f.read()
 
     print(insert_info)
 
-    new_readme_md_content = re.sub(r'---start---(.|\n)*---end---', insert_info, readme_md_content)
+    # new_readme_md_content = re.sub(r'---start---(.|\n)*---end---', insert_info, readme_md_content)
 
-    with open (os.path.join(os.getcwd(), "README.md"), 'w', encoding='utf-8') as f:
-        f.write(new_readme_md_content)
+    # with open (os.path.join(os.getcwd(), "README.md"), 'w', encoding='utf-8') as f:
+    #     f.write(new_readme_md_content)
 
-    print("==new_readme_md_content==>>", new_readme_md_content)
+    # print("==new_readme_md_content==>>", new_readme_md_content)
 
     return True
 
@@ -275,7 +277,6 @@ def main():
         # 计算md文件的sha1值，并与md_sha1_dic做对比
         sha1_key =  os.path.basename(md)
         sha1_value = get_sha1(md)
-        read_md(md)
         # 如果sha1与md_sha1_dic中记录的相同，则打印：XX文件无需同步;
         if((sha1_key in md_sha1_dic.keys()) and (sha1_value == md_sha1_dic[sha1_key])):
             print(md+"无需同步")
@@ -301,6 +302,6 @@ def main():
     # # 4. 重建md_sha1_dic
     # rebuild_md_sha1_dic(os.path.join(os.getcwd(), ".md_sha1"), os.path.join(os.getcwd(), "_posts"))
     # # 5. 将链接信息写入insert_index_info_in_readme
-    # insert_index_info_in_readme()
+    insert_index_info_in_readme(title_id_dic)
 
 main()
